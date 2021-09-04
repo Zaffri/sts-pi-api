8330b827b5e7884715e7476b59ff9c80097a470b
Steven Morrison <smorrison.dev@gmail.com>
2021-09-04 23:25:00 +0100

    SM: initial commit (explorerhat test)

---
+++ app.py
@@ -0,0 +1,19 @@
+import explorerhat
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/motor")
+def motor():
+    left_speed = request.args.get('left', '')
+    right_speed = request.args.get('right', '')
+    msg = f"Move motor left {left_speed} and right {right_speed}"
+    explorerhat.motor.one.speed(right_speed)
+    explorerhat.motor.two.speed(left_speed)
+    return msg
+
+@app.route("/motor/stop")
+def motor():
+    explorerhat.motor.one.stop()
+    explorerhat.motor.two.stop()
+    return "Stop all motors"
